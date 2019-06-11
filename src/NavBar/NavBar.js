453645461871e048607465e64a4491ac0342681c
liubich <liubich.a@gmail.com>
2019-06-12 00:02:45 +0300

    Add styling for NavBar

--- src/NavBar/NavBar.js
+++ src/NavBar/NavBar.js
@@ -1,18 +1,19 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { withRouter, Link } from 'react-router-dom';
+import './NavBar.css';
 
 const NavBar = () => {
   return (
     <nav className="navbar">
-      <ul>
-        <li>
+      <ul className="navbar__links">
+        <li className="navbar__link">
           <Link to="/">Main</Link>
         </li>
-        <li>
+        <li className="navbar__link">
           <Link to="/authors">Authors</Link>
         </li>
-        <li>
+        <li className="navbar__link">
           <Link to="/posts">Posts</Link>
         </li>
       </ul>
