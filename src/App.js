6731302df968d02015655732489403945c63d2c9
liubich <liubich.a@gmail.com>
2019-06-13 20:24:47 +0300

    Change routing. Move NavBar to App

--- src/App.js
+++ src/App.js
@@ -1,17 +1,23 @@
 import React from 'react';
-import { Route, BrowserRouter as Router } from 'react-router-dom';
+import { Route, BrowserRouter as Router, Switch } from 'react-router-dom';
 import { connect } from 'react-redux';
 import MainPage from './MainPage/MainPage';
 import Users from './Users/Users';
 import Posts from './Posts/Posts';
+import NavBar from './NavBar/NavBar';
 
 const App = () => {
   return (
-    <Router>
-      <Route path="/" exact component={MainPage} />
-      <Route path="/users" component={Users} />
-      <Route path="/posts" component={Posts} />
-    </Router>
+    <>
+      <Router>
+        <NavBar />
+        <Switch>
+          <Route path="/" exact component={MainPage} />
+          <Route path="/users" component={Users} />
+          <Route path="/posts" component={Posts} />
+        </Switch>
+      </Router>
+    </>
   );
 };
 
