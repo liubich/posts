23b1a5c6d5ce0487c302d76adad5fec27d667a52
liubich <liubich.a@gmail.com>
2019-06-14 20:55:13 +0300

    Add displaying of 5 last posts

--- src/MainPage/MainPage.js
+++ src/MainPage/MainPage.js
@@ -1,12 +1,30 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { connect } from 'react-redux';
+import * as actions from '../redux/actions/actions';
 
-const MainPage = () => {
-  return <h2>Main page</h2>;
+const MainPage = props => {
+  useEffect(() => {
+    if (!props.postsData) props.dispatch(actions.GetPosts());
+  });
+  return (
+    <>
+      {props.postsData
+        ? props.postsData.slice(0, 4).map((item, index) => (
+            <div className="jumbotron" key={index}>
+              <h1 className="display-5">{item.title}</h1>
+              <hr className="my-4" />
+              <p className="lead">{item.body}</p>
+            </div>
+          ))
+        : null}
+    </>
+  );
 };
 
-const MapStateToProps = () => {
-  return {};
+const MapStateToProps = state => {
+  return {
+    postsData: state.postsData,
+  };
 };
 
 export default connect(MapStateToProps)(MainPage);
