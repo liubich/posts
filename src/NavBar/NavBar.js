60bbae3916344a1f4e4ec1efe053de3ee8ef418c
liubich <liubich.a@gmail.com>
2019-06-14 20:54:51 +0300

    Add conteiner for better layout

--- src/NavBar/NavBar.js
+++ src/NavBar/NavBar.js
@@ -6,23 +6,25 @@ import './NavBar.css';
 const NavBar = () => {
   return (
     <nav className="navbar navbar-expand-lg fixed-top navbar-dark bg-primary">
-      <ul className="navbar-nav mr-auto">
-        <li className="nav-item">
-          <Link to="/" className="nav-link">
-            Main
-          </Link>
-        </li>
-        <li className="nav-item">
-          <Link to="/users" className="nav-link">
-            Users
-          </Link>
-        </li>
-        <li className="nav-item">
-          <Link to="/posts" className="nav-link">
-            Posts
-          </Link>
-        </li>
-      </ul>
+      <div className="container">
+        <ul className="navbar-nav mr-auto">
+          <li className="nav-item">
+            <Link to="/" className="nav-link">
+              Main
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link to="/users" className="nav-link">
+              Users
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link to="/posts" className="nav-link">
+              Posts
+            </Link>
+          </li>
+        </ul>
+      </div>
     </nav>
   );
 };
