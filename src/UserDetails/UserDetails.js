05f0ea4cff2efe7da985e0999f44f607f6736e67
liubich <liubich.a@gmail.com>
2019-06-17 18:15:14 +0300

    Improve UserDetails component

--- src/UserDetails/UserDetails.js
+++ src/UserDetails/UserDetails.js
@@ -1,13 +1,77 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
+import * as actions from '../redux/actions/actions';
 
 const UserDetails = props => {
-  return <h1>{props.match.params.userId}</h1>;
+  useEffect(() => {
+    if (!props.usersData) props.dispatch(actions.GetUsers());
+  });
+  if (props.usersData) {
+    const userId = parseInt(props.match.params.userId, 10);
+    const userDetails = props.usersData.filter(a => a.id === userId)[0];
+
+    return (
+      <div className="card mb-3">
+        <div className="card bg-light mb-3">
+          <h3 className="card-header">
+            {userDetails.name}
+            <span className="text-muted"> aka {userDetails.username}</span>
+          </h3>
+          <div className="card-header">Contacts:</div>
+          <div className="card-body">
+            <h5 className="card-title">{userDetails.phone}</h5>
+            <p>
+              <a href={`mailto:${userDetails.website}`} className="card-link">
+                {userDetails.email}
+              </a>
+            </p>
+            <a
+              href={`http://${userDetails.website}`}
+              target="_blank"
+              rel="noopener noreferrer"
+              className="card-link"
+            >
+              {userDetails.website}
+            </a>
+          </div>
+        </div>
+        <div className="card bg-light mb-3">
+          <div className="card-header">Address:</div>
+          <div className="card-body">
+            <p className="card-text">
+              {`${userDetails.address.city}, ${
+                userDetails.address.street
+              } str., ${userDetails.address.suite}`}
+            </p>
+            <p className="card-text">{`zip: ${userDetails.address.zipcode}`}</p>
+          </div>
+        </div>
+        <div className="card bg-light mb-3">
+          <div className="card-header">Company</div>
+          <div className="card-body">
+            <h4 className="card-title">{userDetails.company.name}</h4>
+            <p className="card-text">
+              {`Catch phrase: ${userDetails.company.catchPhrase}`}
+            </p>
+            <p className="card-text">{`Business: ${userDetails.company.bs}`}</p>
+          </div>
+        </div>
+      </div>
+    );
+  } else return null;
 };
 
 UserDetails.propTypes = {
   match: PropTypes.object,
+  usersData: PropTypes.array,
+  dispatch: PropTypes.func.isRequired,
 };
 
-export default connect()(UserDetails);
+const mapStateToProps = state => {
+  return {
+    usersData: state.usersData,
+  };
+};
+
+export default connect(mapStateToProps)(UserDetails);
