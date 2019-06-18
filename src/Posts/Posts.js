5e3e3779f99a83224ee0db5ef9e945f2900703f5
liubich <liubich.a@gmail.com>
2019-06-18 12:45:46 +0300

    Replace <a> by React's Link

--- src/Posts/Posts.js
+++ src/Posts/Posts.js
@@ -1,7 +1,7 @@
-import React, { useEffect } from 'react';
-import { connect } from 'react-redux';
-import PropTypes from 'prop-types';
-import * as actions from '../redux/actions/actions';
+import React, { useEffect } from "react";
+import { connect } from "react-redux";
+import PropTypes from "prop-types";
+import * as actions from "../redux/actions/actions";
 
 const Posts = props => {
   useEffect(() => {
@@ -15,15 +15,9 @@ const Posts = props => {
               <h1 className="display-5">{item.title}</h1>
               <hr className="my-4" />
               <p className="lead">{item.body}</p>
-              <p className="lead">
-                <a
-                  className="btn btn-primary btn-lg"
-                  href={`/post/${item.id}`}
-                  role="button"
-                >
-                  Comments
-                </a>
-              </p>
+              <Link className="card-link" to={`/post/${item.id}`}>
+                Comments
+              </Link>
             </div>
           ))
         : null}
@@ -33,12 +27,12 @@ const Posts = props => {
 
 Posts.propTypes = {
   dispatch: PropTypes.func,
-  postsData: PropTypes.array,
+  postsData: PropTypes.array
 };
 
 const MapStateToProps = state => {
   return {
-    postsData: state.postsData,
+    postsData: state.postsData
   };
 };
 
