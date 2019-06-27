936d3f7c27665969ec17387c35910e1829b64406
liubich <liubich.a@gmail.com>
2019-06-27 16:38:38 +0300

    Use Object.values

--- src/Users/Users.js
+++ src/Users/Users.js
@@ -14,28 +14,28 @@ const Users = ({ usersData, dispatch }) => {
         <strong>Active users</strong>
       </h2>
       {usersData
-        ? usersData.map((item, index) => (
-            <div className="card mb-3" key={index}>
-              <h3 className="card-header">
-                {item.name}
-                <span className="text-muted"> aka {item.username}</span>
-              </h3>
-              <div className="card-body">
-                <h5 className="card-title">{item.phone}</h5>
-                <a
-                  href={`mailto:${item.email}`}
-                  className="card-subtitle text-muted"
-                >
-                  {item.email}
-                </a>
-              </div>
-              <div className="card-body">
-                <Link to={`/user/${item.id}`} className="card-link">
-                  User details
-                </Link>
-              </div>
+        ? Object.values(usersData).map((item, index) => (
+          <div className="card mb-3" key={index}>
+            <h3 className="card-header">
+              {item.name}
+              <span className="text-muted"> aka {item.username}</span>
+            </h3>
+            <div className="card-body">
+              <h5 className="card-title">{item.phone}</h5>
+              <a
+                href={`mailto:${item.email}`}
+                className="card-subtitle text-muted"
+              >
+                {item.email}
+              </a>
             </div>
-          ))
+            <div className="card-body">
+              <Link to={`/user/${item.id}`} className="card-link">
+                User details
+                </Link>
+            </div>
+          </div>
+        ))
         : null}
     </>
   );
