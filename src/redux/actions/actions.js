6a09cac25bb5f625c732c6131e970392b899bd77
liubich <liubich.a@gmail.com>
2019-06-27 20:42:46 +0300

    Remove useless function. Rename function getComments

--- src/redux/actions/actions.js
+++ src/redux/actions/actions.js
@@ -7,27 +7,6 @@ export function getUsersAndPosts() {
   };
 }
 
-export function linkPostsAndUsers(data, dispatch) {
-  let [usersData, postsData] = data;
-  postsData = postsData.map(post => {
-    return {
-      ...post,
-      authorUsername: usersData.find(user => user.id === post.userId).username,
-    };
-  });
-  dispatch({
-    type: actionTypes.SAVE_POSTS,
-    postsData,
-  });
-  usersData = usersData.map(user => {
-    return {
-      ...user,
-      postsCount: postsData.filter(post => post.userId === user.id).length,
-    };
-  });
-  dispatch({ type: actionTypes.SAVE_USERS, usersData });
-}
-
 function getPosts(dispatch) {
   fetch('https://jsonplaceholder.typicode.com/posts').then(response =>
     response.json().then(postsData => {
@@ -50,7 +29,7 @@ function getUsers(dispatch) {
   );
 }
 
-export function GetComments(postId) {
+export function getComments(postId) {
   return function(dispatch) {
     fetch(`https://jsonplaceholder.typicode.com/comments?postId=${postId}`)
       .then(response => response.json())
