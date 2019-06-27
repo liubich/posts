936625d962a40360ec04b361ee5439cc3e34c808
liubich <liubich.a@gmail.com>
2019-06-27 16:30:00 +0300

    Using getLastPosts function. Change header

--- src/MainPage/MainPage.js
+++ src/MainPage/MainPage.js
@@ -8,36 +8,37 @@ const MainPage = ({ postsData, usersData, dispatch }) => {
   useEffect(() => {
     if (!postsData || !usersData) dispatch(actions.getUsersAndPosts());
   }, []);
+  const postsNumber = 5;
   return (
     <>
       <h2 className="text-center">
-        <strong>Last 5 posts</strong>
+        <strong>{`Last ${postsNumber} posts`}</strong>
       </h2>
       {postsData
-        ? postsData.slice(0, 5).map((item, index) => (
-            <div className="jumbotron" key={index}>
-              <h6>
-                written by @
+        ? actions.getLastPosts(postsNumber).map((item, index) => (
+          <div className="jumbotron" key={index}>
+            <h6>
+              written by @
                 <Link
-                  className="text-monospace text-dark"
-                  to={`/user/${item.userId}`}
-                >
-                  {item.authorUsername}
-                </Link>
-              </h6>
-              <hr className="my-4" />
-              <h5>{item.title}</h5>
-              <p className="lead">{item.body}</p>
-
-              <Link
-                className="btn btn-primary btn-lg"
-                to={`/post/${item.id}`}
-                role="button"
+                className="text-monospace text-dark"
+                to={`/user/${item.userId}`}
               >
-                View comments
+                {item.authorUsername}
               </Link>
-            </div>
-          ))
+            </h6>
+            <hr className="my-4" />
+            <h5>{item.title}</h5>
+            <p className="lead">{item.body}</p>
+
+            <Link
+              className="btn btn-primary btn-lg"
+              to={`/post/${item.id}`}
+              role="button"
+            >
+              View comments
+              </Link>
+          </div>
+        ))
         : null}
     </>
   );
--- src/Posts/Posts.js
+++ src/Posts/Posts.js
@@ -4,38 +4,40 @@ import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
 import * as actions from "../redux/actions/actions";
 
-const Posts = ({ postsData, dispatch }) => {
+const Posts = ({ postsData, usersData, dispatch }) => {
   useEffect(() => {
     if (!postsData || !usersData) dispatch(actions.getUsersAndPosts());
   }, []);
+  const postsNumber = 10;
+
   return (
     <>
-      <h2 className="text-center">Last 10 posts</h2>
+      <h2 className="text-center">{`Last ${postsNumber} posts`}</h2>
       {postsData
-        ? postsData.map((item, index) => (
-            <div className="jumbotron" key={index}>
-              <h6>
-                written by @
+        ? actions.getLastPosts(postsNumber).map((item, index) => (
+          <div className="jumbotron" key={index}>
+            <h6>
+              written by @
                 <Link
-                  className="text-monospace text-dark"
-                  to={`/user/${item.userId}`}
-                >
-                  {item.authorUsername}
-                </Link>
-              </h6>
-              <hr className="my-4" />
-              <h5>{item.title}</h5>
-              <p className="lead">{item.body}</p>
-
-              <Link
-                className="btn btn-primary btn-lg"
-                to={`/post/${item.id}`}
-                role="button"
+                className="text-monospace text-dark"
+                to={`/user/${item.userId}`}
               >
-                View comments
+                {item.authorUsername}
               </Link>
-            </div>
-          ))
+            </h6>
+            <hr className="my-4" />
+            <h5>{item.title}</h5>
+            <p className="lead">{item.body}</p>
+
+            <Link
+              className="btn btn-primary btn-lg"
+              to={`/post/${item.id}`}
+              role="button"
+            >
+              View comments
+              </Link>
+          </div>
+        ))
         : null}
     </>
   );
