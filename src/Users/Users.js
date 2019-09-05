89fbc51ee47e2e7e347f2f3da79f75774d81eb47
liubich <liubich.a@gmail.com>
2019-09-05 22:12:26 +0300

    Shorter syntax

--- src/Users/Users.js
+++ src/Users/Users.js
@@ -1,8 +1,8 @@
-import React, { useEffect } from 'react';
-import { connect } from 'react-redux';
-import PropTypes from 'prop-types';
-import * as actions from '../redux/actions/actions';
-import { Link } from 'react-router-dom';
+import React, { useEffect } from "react";
+import { connect } from "react-redux";
+import PropTypes from "prop-types";
+import * as actions from "../redux/actions/actions";
+import { Link } from "react-router-dom";
 
 const Users = ({ usersData, dispatch }) => {
   useEffect(() => {
@@ -13,30 +13,29 @@ const Users = ({ usersData, dispatch }) => {
       <h2 className="text-center">
         <strong>Active users</strong>
       </h2>
-      {usersData
-        ? Object.values(usersData).map((item, index) => (
-            <div className="card mb-3" key={index}>
-              <h3 className="card-header">
-                {item.name}
-                <span className="text-muted"> aka {item.username}</span>
-              </h3>
-              <div className="card-body">
-                <h5 className="card-title">{item.phone}</h5>
-                <a
-                  href={`mailto:${item.email}`}
-                  className="card-subtitle text-muted"
-                >
-                  {item.email}
-                </a>
-              </div>
-              <div className="card-body">
-                <Link to={`/user/${item.id}`} className="card-link">
-                  User details
-                </Link>
-              </div>
+      {usersData &&
+        Object.values(usersData).map((item, index) => (
+          <div className="card mb-3" key={index}>
+            <h3 className="card-header">
+              {item.name}
+              <span className="text-muted"> aka {item.username}</span>
+            </h3>
+            <div className="card-body">
+              <h5 className="card-title">{item.phone}</h5>
+              <a
+                href={`mailto:${item.email}`}
+                className="card-subtitle text-muted"
+              >
+                {item.email}
+              </a>
             </div>
-          ))
-        : null}
+            <div className="card-body">
+              <Link to={`/user/${item.id}`} className="card-link">
+                User details
+              </Link>
+            </div>
+          </div>
+        ))}
     </>
   );
 };
